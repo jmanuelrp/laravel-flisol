072cae8bfce96116f3c71b4f622545e6e5cadf0f
JManuel Ruiz <metalfranckie@gmail.com>
2015-04-23 16:15:55 -0500

    Redireccion hacia categorias

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated {
 	{
 		if ($this->auth->check())
 		{
-			return new RedirectResponse(url('/home'));
+			return new RedirectResponse(route('categorias.index'));
 		}
 
 		return $next($request);
