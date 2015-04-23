945e247845657165d3a3e67f9d36d2e5ff32927d
JManuel Ruiz <metalfranckie@gmail.com>
2015-04-23 16:14:56 -0500

    Relacion de usuario con compras

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	public function compras()
+	{
+		return $this->belongsToMany('App\Compra', 'producto_compra', 'compra_id', 'user_id');
+	}
+
 }
